fd02e7faded6ccefae74fca7fd5aac5959a5bf81
Ravindhran Jabaraj <87109840+RavindhranJabarajG@users.noreply.github.com>
2021-07-21 18:32:57 +0530

    Update 5.js

--- 5.js
+++ 5.js
@@ -2,24 +2,27 @@
 var fs = require('fs');
 var replace = require('replace');
 var glob = require('glob');
+var exist = require('./1.js');
+var filename = "fileA.txt";
 
-try{
-    glob('fileA.txt', function(err, files) {
-        if (err) {
-             throw err;
-             }
-        files.forEach(function(file, index, array) {
-              console.log(file + ' exists');
-              console.log(fs.readFileSync(file, 'utf8'));
-              replace({
-                  regex: 'nice', replacement: 'good', paths: [file], recursive: true, silent: true
-              });
-              console.log('words replaced.');
-              console.log(fs.readFileSync(file, 'utf8'));
+if(!exist.check(filename)){
+    try{
+        glob(filename, function(err, files) {
+            if (err) {
+                throw err;
+            }
+            files.forEach(function(file, index, array) {
+                console.log(file + ' exists');
+                console.log(fs.readFileSync(file, 'utf8'));
+                replace({
+                    regex: 'nice', replacement: 'good', paths: [file], recursive: true, silent: true
+                });
+                console.log('words replaced.');
+                console.log(fs.readFileSync(file, 'utf8'));
+            });
         });
-    });
-}
-
-catch(err){
-    console.log('File not found');
+    }
+    catch(err){
+        console.log('Unable to find words');
+    }
 }
