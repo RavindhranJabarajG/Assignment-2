735e552f16426e5ae1a3798b8fb4a7ad75187a13
Ravindhran Jabaraj <87109840+RavindhranJabarajG@users.noreply.github.com>
2021-07-21 18:36:17 +0530

    Update 8.js

--- 8.js
+++ 8.js
@@ -1,26 +1,30 @@
-//If the text is already present append text on the next line.
+//8.If the text is already present append text on the next line.
 var fs = require('fs');
 var replace = require('replace');
 var glob = require('glob');
+var exist = require("./1.js");
+var filename = "fileB.txt";
 
-try{
-    glob('fileA.txt', function(err, files) {
-        if (err) {
-             throw err;
-             }
-        files.forEach(function(file, index, array) {
-              console.log(file + ' exists');
-              fs.appendFile('fileA.txt',"Have fun coding!!",function(err){
-                  if(err){
-                      console.log(err);
-                  } else{
-                      console.log("File appended");
-                  }
-              });
+if(!exist.check(filename)){
+    try{
+        glob('fileA.txt', function(err, files) {
+            if (err) {
+                throw err;
+                }
+            files.forEach(function(file, index, array) {
+                console.log(file + ' exists');
+                fs.appendFile('fileA.txt',"\nHave fun coding!!",function(err){
+                    if(err){
+                        console.log(err);
+                    } else{
+                    console.log("File appended");
+                    }
+                });
+            });
         });
-    });
-}
-
-catch(err){
-    console.log('File not found');
+    }    
+    
+    catch(err){
+        console.log('File not found');
+    }
 }
