a00801085765e10dd637015839714b88c3481401
Ravindhran Jabaraj <87109840+RavindhranJabarajG@users.noreply.github.com>
2021-07-21 17:50:44 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,13 +1,17 @@
 //1. Check if the file is present or not.
-var fs= require("fs");
+var fs = require("fs");
 
+function check(filename){
+    fs.access(filename, function(err){
+        if(err){
+            console.log("File does not exist.");
+            return false;
+        }
+        else{
+            return true;
+        }
+    });
+}
 
-fs.access('fileA.txt', (err) => {
-    if(err){
-        console.log("File does not exist.");
-    }
-    else{
-        console.log("File exists");
-    }
-})
+module.exports = {check};
 
