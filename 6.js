64b1b4fef2eee23bcefe25e77d3ad99d50f8c3ec
Ravindhran Jabaraj <87109840+RavindhranJabarajG@users.noreply.github.com>
2021-07-21 18:02:06 +0530

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,12 +1,31 @@
 //6. Write it back on the file.
+var fs= require("fs");
+var exist = require("./1.js");
+var filename = "fileB.txt";
+var existsfile = exist.check(filename);
 
-var fs= require('fs');
+const writefile = () =>
+{
+    fs.copyFile(filename,'fileB.txt',function(err) {
+        //contents of fileB is written back on the same file. Hence, same output.
+        if(err){
+            console.log("An error occured")
+        }
+        else{
+            console.log("contents written back on the same file");
+            }
+    });  
+};
 
-fs.copyFile('fileA.txt','fileA.txt',function(err){
-    if(err){
-        console.log("couldn't write back on the file");
+const checkfile = (filename,callback) =>
+{
+    const fileCheck = (existsfile,callback);
+    if(fileCheck){
+        callback();
     }
     else{
-        console.log("wriiten on the same file");
+        console.log("file not found");
     }
-});
+};
+
+checkfile(filename,writefile);
